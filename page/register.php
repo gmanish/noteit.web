b1f3f6dc8ec6b23adada6700d45bce5e3d038af3
Manish Gupta <gmanish@gmail.com>
2011-12-08 08:11:25 +0530

    madness continued

--- page/register.php
+++
@@ -1,8 +0,0 @@
-<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../controller/ControllerDefines.php") ?>
-<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/htmlHeader.tphp") ?>
-<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/noteitBodyBegin.tphp") ?>
-<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/noteitheader.tphp") ?>
-<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/Register.tphp") ?>
-<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/noteitfooter.tphp") ?>
-<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/noteitBodyEnd.tphp") ?>
-<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/htmlfooter.tphp") ?>
