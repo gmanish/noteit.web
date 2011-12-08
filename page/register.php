843a21da44523b9c5ff49b503b07c74ac0e6689a
Manish Gupta <gmanish@gmail.com>
2011-12-08 14:30:03 +0530

    Restoring register.php which i had inadvertently delete and fixing a bug in registration.

---
+++ page/register.php
@@ -0,0 +1,8 @@
+<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../controller/controllerdefines.php") ?>
+<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/htmlheader.tphp") ?>
+<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/noteitbodybegin.tphp") ?>
+<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/noteitheader.tphp") ?>
+<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/register.tphp") ?>
+<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/noteitfooter.tphp") ?>
+<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/noteitbodyend.tphp") ?>
+<?php require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . ("../view/htmlfooter.tphp") ?>
